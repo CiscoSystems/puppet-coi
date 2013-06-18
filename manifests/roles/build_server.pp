7d0a4671cad4c66fb2ef7476f05f5f330806b9ab
Dan Bode <bodepd@gmail.com>
2013-06-17 22:35:34 -0700

    Update coi build role
    Previous, most of the profiles for the build
    role were commented out.
    
    This patch uncomments them.

--- manifests/roles/build_server.pp
+++ manifests/roles/build_server.pp
@@ -2,8 +2,8 @@
 # this is the COI build server.
 #
 class coi::roles::build_server {
-  #include coi::profiles::puppet::master
-  #include coi::profiles::monitoring_server
+  include coi::profiles::puppet::master
+  include coi::profiles::monitoring_server
   include coi::profiles::cobbler_server
-  #include coi::profiles::cache_server
+  include coi::profiles::cache_server
 }
