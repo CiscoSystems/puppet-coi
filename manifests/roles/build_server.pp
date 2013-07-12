0b2b7419c41876c73c78924204bcaefd1f9b6604
Pradeep Kilambi <pkilambi@cisco.com>
2013-07-12 18:27:32 -0400

    Add knobs for cobbler, cache server etc..

--- manifests/roles/build_server.pp
+++ manifests/roles/build_server.pp
@@ -1,9 +1,23 @@
 #
 # this is the COI build server.
 #
-class coi::roles::build_server {
+class coi::roles::build_server (
+  $enable_cobbler      = hiera('enable_cobbler', $::coi::roles::params::enable_cobbler),
+  $enable_cache        = hiera('enable_cache', $::coi::roles::params::enable_cache),
+  $enable_monitoring   = hiera('enable_monitoring', true),
+  ){
   include coi::profiles::puppet::master
-  include coi::profiles::monitoring_server
-  include coi::profiles::cobbler_server
-  include coi::profiles::cache_server
+
+  if ($enable_monitoring) {
+    include coi::profiles::monitoring_server
+  }
+
+  if ($enable_cobbler) {
+    include coi::profiles::cobbler_server
+  }
+
+  if ($enable_cache) {
+    include coi::profiles::cache_server
+  }
+
 }
