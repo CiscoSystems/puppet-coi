70ce77b6a2925b3e34afedad709cfddcd087de06
Dan Bode <bodepd@gmail.com>
2013-09-13 16:05:25 -0700

    Remove deprecated files
    This commit removes files that have been
    deprecated by the data_refactor.

--- manifests/roles/compute.pp
+++
@@ -1,7 +0,0 @@
-class coi::roles::compute {
-
-  include kickstack::compute
-  include coi::profiles::openstack::base
-  include naginator::compute_target
-
-}
--- manifests/roles/controller.pp
+++
@@ -1,7 +0,0 @@
-class coi::roles::controller {
-  include kickstack::controller
-  include kickstack::network_controller
-  include coi::profiles::openstack::test_file
-  include coi::profiles::openstack::base
-  include naginator::control_target
-}
