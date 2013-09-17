2a80059c04686023c966bdda64c063acea42224c
Dan Bode <bodepd@gmail.com>
2013-09-16 22:58:42 -0700

    Remove swift code
    As we move to the data model, I will be moving
    more and more code out of the coi module.
    
    This commit removes all of the swift code.

--- manifests/roles/swift_proxy.pp
+++
@@ -1,6 +0,0 @@
-class coi::roles::swift_proxy {
-
-  include coi::profiles::openstack::base
-  include coi::profiles::openstack::swift::proxy
-
-}
--- manifests/roles/swift_storage.pp
+++
@@ -1,6 +0,0 @@
-class coi::roles::swift_storage {
-
-  include coi::profiles::openstack::base
-  include coi::profiles::openstack::swift::storage
-
-}
